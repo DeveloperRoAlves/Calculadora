74261f03cac993dbd5535066db1ab7c5d1c9648c
DeveloperRoAlves <rospereira.stos@gmail.com>
2022-09-28 23:47:03 -0300

    commit inicial

---
+++ ConsoleAppRosangela/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppRosangela
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1;
+            int num2;
+
+            Console.WriteLine(" ****** Programa da Rosangela ****** \n\n");
+            Console.WriteLine(" ---  Multiplicação de dois Números --- \n\n");
+
+            Console.WriteLine("Informe um número: ");
+            num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe o segundo número: ");
+            num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Resultado: {num1*num2}");
+
+
+            Console.ReadLine();
+        }
+    }
+}
